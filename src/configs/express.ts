998fe47bb88c4a281f64b3feca97c2c715b2572f
chosakuken <shunpc2018@outlook.jp>
2026-09-11 14:26:29 +0900

    feat: view engine の設定

--- src/configs/express.ts
+++ src/configs/express.ts
@@ -8,5 +8,7 @@ const APP_DIR = path.join(path.dirname(fileURLToPath(import.meta.url)), "..");
 // 設定済み Express インスタンスを返す
 export function setupExpress() {
   const app = express();
+  app.set("view engine", "ejs");
+  app.set("views", path.join(APP_DIR, "views"));
   return app;
 }
